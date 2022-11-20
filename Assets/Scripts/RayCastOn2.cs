2577696ab3bb3adc447196de675e11f075eb8ac8
gitmsx <gitmsx@mail.ru>
2022-11-20 16:47:20 +0300

    Saved on 20.11.2022-16:47:19,80

--- Assets/Scripts/RayCastOn2.cs
+++ Assets/Scripts/RayCastOn2.cs
@@ -16,7 +16,10 @@ public class RayCastOn2 : MonoBehaviour
 
 
     [HideInInspector] private Text Text__info001;
+    public GameObject BulletPF;
+    private GameObject bulletClone;
 
+    public float bulletSpeed=100;
     public Transform Pointer;  // collide with Ray
     public Transform Pointer2;  // collide with Ray
                                        //  [HideInInspector] public event Ray_Delegate ray_event;
@@ -25,7 +28,15 @@ public class RayCastOn2 : MonoBehaviour
     private float CircleS = 0;
 
 
+    private Ray[] ray = new Ray[4];
+    
 
+
+   
+    private  Vector3[] DirectionM = new Vector3[4];
+
+
+    
 
 
 
@@ -61,13 +72,18 @@ public class RayCastOn2 : MonoBehaviour
     {
         RaycastHit hit;
 
-        Ray[] ray = new Ray[4];
+        DirectionM[0] = Vector3.forward;
+        DirectionM[1] = Vector3.right;
+        DirectionM[2] = -Vector3.forward;
+        DirectionM[3] = -Vector3.right;
+
 
         ray[0] = new Ray(transform.position, Vector3.forward * 30);
+        ray[0] = new Ray(transform.position, DirectionM[3] * 30);
+
         ray[1] = new Ray(transform.position, Vector3.right * 30);
         ray[2] = new Ray(transform.position, -Vector3.forward * 30);
         ray[3] = new Ray(transform.position, -Vector3.right * 30);
-
 
 
         for (int i = 0; i < 4; i++)
@@ -85,6 +101,8 @@ public class RayCastOn2 : MonoBehaviour
                         + " hit.point " + hit.point.ToString();
 
                     Pointer.position = hit.point;
+
+                    Fire(i);
                 }
 
 
@@ -101,6 +119,22 @@ public class RayCastOn2 : MonoBehaviour
 
 
     }
+
+    void Fire(int i)
+    {
+
+        bulletClone = Instantiate(BulletPF, transform.position, transform.rotation);
+        
+        bulletClone.GetComponent<Rigidbody>().velocity = DirectionM[i] * bulletSpeed;
+
+
+
+        Destroy(bulletClone, 5.5f);
+        
+         
+
+    }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/bullet8.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+public class bullet8 : MonoBehaviour 
+ {
+
+    public float StartTime=0;
+    public float DestroyTime = 0.2f;
+    public float SShift1 = 0.02f;
+    public float SShift2 = 0.02f;
+    private GameObject bulletClone;
+    
+
+    public float bulletSpeed = 70;
+    //public Rigidbody bullet;
+    public GameObject BulletPF;
+    public GameObject SperePF;
+    private List<GameObject> listObj = new List <GameObject>();
+
+
+    private void Start()
+    {
+        for (int i = 0; i < 22; i++)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+
+        bulletClone = Instantiate(BulletPF, transform.position, transform.rotation);
+        bulletClone.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+
+       
+        var material_obj = bulletClone.GetComponent<Material>();
+
+        listObj.Add(bulletClone);
+
+        // Debug.Log(color_obj);
+       // Debug.Log(material_obj);
+
+        var x1 = (float)Random.Range(-3, 5);
+        var y1 = (float)Random.Range(-3, 5);
+        var z1 = (float)Random.Range(-3, 5);
+
+        Vector3 posi1 = new Vector3(x1, y1, z1);
+        Destroy(bulletClone, 5.5f);
+
+
+
+
+    }
+ 
+
+    void make10units()
+    {
+      //  Debug.Log(listObj.Count);
+        // for (int i = 0; i < listObj.Count; i++)
+
+        int i = 1;
+        foreach (var obj in listObj)
+        {
+            i++;
+      ///      Destroy(obj, i * DestroyTime / 10);
+
+        }
+
+
+
+    }
+
+    void MotionMod()
+    {
+
+
+        //foreach (var obj in listObj)
+        //{
+
+        //    var x1 = (float)Random.Range(-SShift1, SShift2);
+        //    var y1 = (float)Random.Range(-SShift1, SShift2);
+        //    var z1 = (float)Random.Range(-SShift1, SShift2);
+
+        //    var objtransform = obj.transform;
+        //    // objtransform.localScale = new Vector3(x1, y1, z1);
+        //    objtransform.localPosition = new Vector3(obj.transform.position.x + x1, obj.transform.position.y + y1, obj.transform.position.z + z1);
+
+
+
+
+        //}
+
+
+    }
+
+    void Update () 
+     {
+
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            MotionMod();
+
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            make10units();
+
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+            
+        }
+
+        MotionMod();
+    }
+}
